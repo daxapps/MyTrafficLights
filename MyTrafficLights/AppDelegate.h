0d1b72c1468fa9d752e5ce1e2a4ecfc5f47d6d36
daxapps <daxapps777@gmail.com>
2017-03-10 19:06:17 -0600

    Initial Commit

---
+++ MyTrafficLights/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTrafficLights
+//
+//  Created by Jason Crawford on 3/10/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTrafficLights/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTrafficLights
+//
+//  Created by Jason Crawford on 3/10/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
