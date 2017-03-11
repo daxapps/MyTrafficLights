64365bbee3bc18d9f4c3e6a5894e56979086b3a8
daxapps <daxapps777@gmail.com>
2017-03-10 20:02:17 -0600

    Use NSTimer to create stopwatch game

--- MyTrafficLights/ViewController.m
+++ MyTrafficLights/ViewController.m
@@ -16,7 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    scoreInt = 0;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+    
+    
 }
 
 
@@ -26,4 +30,70 @@
 }
 
 
+- (IBAction)startStop:(id)sender {
+    
+    if (scoreInt == 0) {
+        
+        timerInt = 3;
+        
+        self.trafficLight.image = [UIImage imageNamed:@"TrafficLight.png"];
+        
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startCounter) userInfo:nil repeats:YES];
+        
+        self.startButton.enabled = NO;
+        [self.startButton setTitle:@"" forState:UIControlStateNormal];
+        
+    } else {
+        
+        [scoreTimer invalidate];
+        
+        [self.startButton setTitle:@"Restart" forState:UIControlStateNormal];
+        
+    }
+    
+    if (timerInt == 0) {
+        
+        scoreInt = 0;
+        
+    }
+}
+
+-(void)startCounter {
+    
+    timerInt -= 1;
+    
+    if (timerInt == 2) {
+        
+        self.trafficLight.image = [UIImage imageNamed:@"TrafficLight3.png"];
+        
+    } else if (timerInt == 1) {
+        
+        self.trafficLight.image = [UIImage imageNamed:@"TrafficLight2.png"];
+        
+    } else if (timerInt == 0) {
+        
+        self.trafficLight.image = [UIImage imageNamed:@"TrafficLight1.png"];
+        
+        [timer invalidate];
+        
+        [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+        self.startButton.enabled = YES;
+        
+        scoreTimer = [NSTimer scheduledTimerWithTimeInterval:0.0001 target:self selector:@selector(scoreCounter) userInfo:nil repeats:YES];
+        
+    }
+}
+
+-(void)scoreCounter {
+    
+    scoreInt += 1;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+    
+}
+
+
+
 @end
+
+
+
